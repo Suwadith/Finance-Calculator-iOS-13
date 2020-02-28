5d092c72e898a20c37964aed0b8e25abd9ce27c0
Suwadith <suwadith.srithar@gmail.com>
2020-02-28 10:05:04 +0530

    first commit

---
+++ FinanceCalculator/Controllers/MortgageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FinanceCalculator
+//
+//  Created by Suwadith on 2/27/20.
+//  Copyright © 2020 Suwadith. All rights reserved.
+//
+
+import UIKit
+
+class MortgageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
