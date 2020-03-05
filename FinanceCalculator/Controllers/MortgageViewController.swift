967a7ee26f6cb605baf6e4a2d68d519237ca293c
Suwadith <suwadith.srithar@gmail.com>
2020-03-06 03:55:22 +0530

    Keyboard hiding text fields fixed

--- FinanceCalculator/Controllers/MortgageViewController.swift
+++ FinanceCalculator/Controllers/MortgageViewController.swift
@@ -14,13 +14,13 @@ enum MortgageUnits: Int {
 
 class MortgageViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var loanAmountField: UITextField!
     @IBOutlet weak var interestField: UITextField!
     @IBOutlet weak var paymentField: UITextField!
     @IBOutlet weak var numberOfYearsField: UITextField!
     
     let keyboardView = KeyboardController(frame: CGRect(x: 0, y: 0, width: 0, height: 300))
-    
     
     var mortgage: Mortgage = Mortgage(loanAmount: 0.0, interest: 0.0, payment: 0.0, numberOfYears: 0.0)
     
@@ -31,8 +31,31 @@ class MortgageViewController: UIViewController, UITextFieldDelegate {
         self.loadDefaultsData("MortgageHistory")
         self.customizeTextFields()
         hideKeyboardWhenTappedAround()
+        addKeyboardEventListeners()
+        
     }
     
+    
+    func addKeyboardEventListeners() {
+        ///listening to keyboard events
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    
+    @objc func keyboardWillChange(notification: Notification) {
+        
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {return}
+
+        ///scroll the view and prevent hiding the current selected text field
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name ==  UIResponder.keyboardWillChangeFrameNotification {
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardRect.height, right: 0)
+            ///get back to the deafult position when tapped away
+        } else {
+            scrollView.contentInset = UIEdgeInsets.zero
+        }
+       }
     
     func loadDefaultsData(_ historyKey :String) {
         let defaults = UserDefaults.standard
